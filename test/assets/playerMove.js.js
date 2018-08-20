8a001732141e0e0b12e1db863635a7b71503417b
韩天宇 <hantianyu@dangdang.com>
2018-08-20 20:05:07 +0800

    first push

---
+++ test/assets/playerMove.js.js
@@ -0,0 +1,56 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+const MoveState = cc.Enum({
+    None: -1,
+    Stand: -1,
+    Up: -1,
+    Right: -1,
+    Down: -1,
+    Left: -1
+});
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+      moveSpeed:0,
+      anim: {
+          default: null,
+          type: cc.Animation
+      },
+      statics: {
+          MoveState: MoveState
+      },
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
